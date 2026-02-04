9721fb99f78c8702ecc9ed3906655b00f47a7801
Nattapong <nattapong.lep@ku.th>
2026-02-04 10:57:31 +0700

    Initial role permission permission_role db

--- app/con_sqlalchemy.py
+++ app/con_sqlalchemy.py
@@ -20,10 +20,27 @@ class User(AuditMixin):
     __tablename__ = "m_user"
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    role_id = db.Column(db.Integer, default=0, nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('m_role.role_id', ondelete='SET NULL', onupdate='CASCADE'))
     password = db.Column(db.String(200), nullable=False)
 
 class Tokenlist(BaseModel):
     __tablename__ = "t_token_list"
     id = db.Column(db.Integer, primary_key=True)
     jwt_id = db.Column(db.String(255), unique=True, nullable=False)
+
+class Role(BaseModel):
+    __tablename__ = "m_role"
+    role_id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(200))
+
+class Permission(BaseModel):
+    __tablename__ = "m_permission"
+    permission_id = db.Column(db.Integer, primary_key=True)
+    permission_code = db.Column(db.String(80), unique=True, nullable=False)
+    permission_group = db.Column(db.String(80), nullable=False)
+
+class RolePermission(BaseModel):
+    __tablename__ = "m_role_permission"
+    role_id = db.Column(db.Integer, db.ForeignKey('m_role.role_id'), primary_key=True, nullable=False)
+    permission_id = db.Column(db.Integer, db.ForeignKey('m_permission.permission_id'), primary_key=True, nullable=False)
