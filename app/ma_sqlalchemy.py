4ef4ed94ed78b08c3e1b92511edc6c9ca810ecb4
Nattapong <nattapong.lep@ku.th>
2026-02-12 13:58:49 +0700

    Add relationship between WorkPhase and WorkOrder, and include sales_item in WorkOrderSchema

--- app/ma_sqlalchemy.py
+++ app/ma_sqlalchemy.py
@@ -80,6 +80,7 @@ class WorkOrderSchema(Schema):
     created_date = fields.DateTime()
     status = fields.String()
     current_phase = fields.Nested(WorkPhaseSchema())
+    sales_item = fields.List(fields.Nested(SalesItemSchema()))
 
 class MaterialListSchema(Schema):
     material_list_id = fields.Integer()
