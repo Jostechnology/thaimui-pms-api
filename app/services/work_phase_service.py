b1861e0a079bd8ca98b06f3449882e4616550df7
Nattapong <nattapong.lep@ku.th>
2026-02-16 10:52:11 +0700

    Refactor create_work_phase to retrieve work_order_id from items and update work_order status to "Working"; update wsgi.py to run app with debug settings

--- app/services/work_phase_service.py
+++ app/services/work_phase_service.py
@@ -7,11 +7,13 @@ from app.app import db
 def create_work_phase(data):
     try:
         work_phases = []
-        work_order = work_order_repository.get_work_order_by_id(data.get("work_order_id"))
+        items = data.get("items", [])
+        work_order_id = items[0].get("work_order_id") if items else None
+        work_order = work_order_repository.get_work_order_by_id(work_order_id)
         work_order.status = "Working"
-        for item in data.get("items", []):
+        for item in items:
             work_phase = WorkPhase(
-                work_order_id=item.get("work_order_id"),
+                work_order_id=work_order_id,
                 phase_name=item.get("phase_name"),
                 start_date=item.get("start_date"),
             )
