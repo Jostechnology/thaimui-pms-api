3de31a7141588836129b2233d5dfd64c57a32a2a
campishome <campnapat007@gmail.com>
2026-02-17 10:17:06 +0700

    add filter in get_all_work_orders

--- app/repositories/work_order_repository.py
+++ app/repositories/work_order_repository.py
@@ -1,11 +1,13 @@
 from app.con_sqlalchemy import WorkOrder, SalesItem
 from app.app import db
 
-def get_all_work_orders(page, limit, search):
+def get_all_work_orders(page, limit, search,filter):
     try:
         query = WorkOrder.query
         if search:
             query = query.filter(WorkOrder.doc_num.ilike(f"%{search}%"))
+        if filter:
+            query = query.filter(WorkOrder.status == filter)
         query = query.paginate(page=page, per_page=limit, error_out=False)
         return {"items": query.items, "total_pages": query.pages}
     except Exception:
