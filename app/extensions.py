74aaa7b2743de9af2369911bdc0934f570f0c94e
jaoaurai <jaoaurai@gmail.com>
2026-04-08 17:31:43 +0700

    edit

--- app/extensions.py
+++ app/extensions.py
@@ -26,7 +26,7 @@ def init_document_generator_service(DOCUMENT_GENERATOR_URL):
 
 def init_storage_service(endpoint, access_key, secret_key, bucket, secure=False):
     from app.services import storage_service
-    storage_service.init_storage(endpoint, access_key, secret_key, bucket, secure)
+    storage_service.init_storage(endpoint, access_key, secret_key, bucket, False)
 
 
 def init_cache_service(redis_url: str):
