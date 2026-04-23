e19b30844ed7cc0017e5eab6c4beb00558e49aec
jaoaurai <jaoaurai@gmail.com>
2026-04-23 11:32:19 +0700

    added all branch access to default user

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # flask db upgrade
-flask seed-apply --path /seeds/permissions.yml
+flask seed-apply --path /seeds/permissions.yml # --prune
 
 exec gunicorn --config gunicorn_config.py --log-level info --access-logfile - wsgi:app
